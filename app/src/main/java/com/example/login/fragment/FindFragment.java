8f0cc593e99de980b360bf93a7ac6b40e7ac0a53
xiaohua <daaoao51@gmail.com>
2024-02-25 17:49:00 +0800

    First commit.

---
+++ app/src/main/java/com/example/login/fragment/FindFragment.java
@@ -0,0 +1,35 @@
+package com.example.login.fragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.login.R;
+
+
+public class FindFragment extends Fragment {
+    private View root;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        if (root == null){
+            root = inflater.inflate(R.layout.fragment_find, container, false);
+        }
+
+
+        return root;
+    }
+}
